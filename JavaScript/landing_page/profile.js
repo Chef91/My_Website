4d7c48bb91c9d2c070399ee8263ad396d3a64e3e
Chef91 <chefgraham1991@gmail.com>
2025-07-14 21:38:41 -0400

    small update
    small update being pushed

--- JavaScript/landing_page/profile.js
+++ JavaScript/landing_page/profile.js
@@ -1,7 +1,9 @@
+// landing page profile cards
 function updateCard(imageSrc, text) {
   document.getElementById('cardImage').src = imageSrc;
   document.getElementById('cardText').innerHTML = profile.text;
 }
+//Profiles
 const profiles = {
   profileChef: {
     image: './images/landing_images/profiles/chef.jpg',
@@ -9,38 +11,41 @@ const profiles = {
     Wesley has been programming for just over two years. He studies full-stack, but his strongest skills are currently in Front-End Development. He likes
     to program mostly in JavaScript, Python, and PHP right now. You can look below to see his skills:`,
     heading: 'Wesleys Skills',
-    image1: '<i class="fa-brands fa-html5 icon-custom"></i>',
-    image2: '<i class="fa-brands fa-css3-alt icon-custom"></i>',
-    image3: '<i class="fa-brands fa-js icon-custom"></i>',
-    image4: '<i class="fa-brands fa-php icon-custom"></i>',
-    image5: '<i class="fa-brands fa-react icon-custom"></i>'
+    image1: '<i class="fa-brands fa-html5 fa-3x "></i>',
+    image2: '<i class="fa-brands fa-css3-alt fa-3x"></i>',
+    image3: '<i class="fa-brands fa-js fa-3x"></i>',
+    image4: '<i class="fa-brands fa-php fa-3x"></i>',
+    image5: '<i class="fa-brands fa-react fa-3x"></i>'
   },
+  // Placeholder
   profileJohn: {
     image: './images/landing_images/profiles/johndoe.jpg',
     text: `This is John Doe, our senior back-end developer. John has been with the company for several years now. He is in charge of all of our back-end operations.
     This means he deals with a lot of programming languages like Python, PHP, MySql, and so on. He's a reliable part of the team because he takes care of all the things
     you don't see when you are looking at our website.`,
     heading: 'Johns skills',
-    image1: '<i class="fa-brands fa-php icon-custom"></i>',
-    image2: '<i class="fa-brands fa-python icon-custom"></i>',
-    image3: '<i class="fa-brands fa-java icon-custom"></i>',
-    image4: '<i class="fa-brands fa-golang icon-custom"></i>',
+    image1: '<i class="fa-brands fa-php fa-3x"></i>',
+    image2: '<i class="fa-brands fa-python fa-3x"></i>',
+    image3: '<i class="fa-brands fa-java fa-3x"></i>',
+    image4: '<i class="fa-brands fa-golang fa-3x"></i>',
     image5: ''
   },
+  // Placeholder
   profileJane: {
     image: './images/landing_images/profiles/johndoe.jpg',
     text: `This is John Doe, our senior back-end developer. John has been with the company for several years now. He is in charge of all of our back-end operations.
     This means he deals with a lot of programming languages like Python, PHP, MySql, and so on. He's a reliable part of the team because he takes care of all the things
     you don't see when you are looking at our website.`,
     heading: 'Johns skills',
-    image1: '<i class="fa-brands fa-html5 icon-custom"></i>',
-    image2: '<i class="fa-brands fa-css3-alt icon-custom"></i>',
-    image3: '<i class="fa-brands fa-js icon-custom"></i>',
-    image4: '<i class="fa-brands fa-react icon-custom"></i>',
+    image1: '<i class="fa-brands fa-html5 fa-3x"></i>',
+    image2: '<i class="fa-brands fa-css3-alt fa-3x"></i>',
+    image3: '<i class="fa-brands fa-js fa-3x"></i>',
+    image4: '<i class="fa-brands fa-react fa-3x"></i>',
     image5: ''
   },
 };
-
+// Function runs profile cards on website
+// cardSkills are the programming language icons
 function showProfile(profileKey) {
   const profile = profiles[profileKey];
   document.getElementById('cardImage').src = profile.image;
